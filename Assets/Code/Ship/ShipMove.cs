b53a6f1820833e8c0a77e9b5654b4053362b1492
German Krasnikov <german.krasnikov@gmail.com>
2022-12-12 14:38:41 +0100

    ShipMove implementation switched to moving to random position

--- Assets/Code/Ship/ShipMove.cs
+++ Assets/Code/Ship/ShipMove.cs
@@ -1,38 +1,45 @@
 ﻿using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
+using UnityEngine.Serialization;
 
 namespace Code.Ship
 {
     public class ShipMove : MonoBehaviour
     {
-        //public CharacterController CharacterController;
-        public float MovementSpeed;
-        //private IInputService _inputService;
-        private Camera _camera;
+        public Vector3 TargetPosition;
+        public float Speed = 2;
+        public float RotationSpeed = 20;
 
         private void Awake()
         {
-            //_inputService = AllServices.Container.Single<IInputService>();
-            _camera = Camera.main;
+            CreateNewTarget();
         }
 
         private void LateUpdate()
         {
-            Vector3 movementVector = Vector3.zero;
-            transform.Rotate(0, 10 * Time.deltaTime, 0);
-            //if (_inputService.Axis.sqrMagnitude > Constants.Epsilon)
-            {
-                //movementVector = _camera.transform.TransformDirection(_inputService.Axis);
-                movementVector = transform.TransformDirection(new Vector3(0.1f, 0, 0.1f));
-                movementVector.y = 0;
-                movementVector.Normalize();
+            MoveToTarget();
+            RotateToTarget();
+            if (RichTarget())
+                CreateNewTarget();
+        }
 
-                //transform.forward = movementVector;
-            }
+        private void CreateNewTarget()
+        {
+            TargetPosition = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 10));
+        }
 
-            transform.Translate(transform.forward * (MovementSpeed * Time.deltaTime));
-            //movementVector += Physics.gravity;
-            //CharacterController.Move(MovementSpeed * movementVector * Time.deltaTime);
+        private bool RichTarget() => Vector3.Distance(TargetPosition, transform.position) < 0.1f;
+
+        private void MoveToTarget()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, TargetPosition, Speed * Time.deltaTime);
+        }
+
+        private void RotateToTarget()
+        {
+            Vector3 dir = TargetPosition - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * RotationSpeed).eulerAngles;
+            transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
         }
     }
 }
